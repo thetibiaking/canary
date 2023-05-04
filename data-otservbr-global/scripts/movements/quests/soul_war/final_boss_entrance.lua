2650ab2325ed806d5a0b3c18756f41d9fe1f45b5
Rafhael Oliveira <74227915+Underewarrr@users.noreply.github.com>
2023-05-04 05:12:53 +0000

    Dev Refac: final_boss_entrance Soulwar new Lib functions

--- data-otservbr-global/scripts/movements/quests/soul_war/final_boss_entrance.lua
+++ data-otservbr-global/scripts/movements/quests/soul_war/final_boss_entrance.lua
@@ -1,18 +1,23 @@
 local soulWarFinalBoss = MoveEvent()
 
 function soulWarFinalBoss.onStepIn(creature, item, position, fromPosition)
-	local player = creature:getPlayer()
-	if not player then
-		return true
-	end
+    local player = creature:getPlayer()
+    if not player then
+        return true
+    end
 
-    
+    if not player:hasAllCurses() then
+        player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You must have all 5 curses to enter the final boss room.")
+        player:getPosition():sendMagicEffect(CONST_ME_POFF)
+        player:teleportTo(fromPosition, true)
+        return true
+    end
 
-	player:teleportTo(fromPosition, true)
-	player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
-	return true
+    -- Player has all 5 curses, allow them to enter the final boss room
+    player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+    return true
 end
 
 soulWarFinalBoss:type("stepin")
-soulWarFinalBoss:aid(12100)
+soulWarFinalBoss:aid(13300)
 soulWarFinalBoss:register()
